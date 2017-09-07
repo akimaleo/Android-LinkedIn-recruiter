fcad8ddf68b551a06a8f7669d673f85becd54ac9
Yaroslav <akimaleo@ya.ru>
2017-09-07 13:48:02 +0300

    Reading from storage/writing in storage and input into fields.

---
+++ app/src/main/java/com/letit0or1/akimaleo/linkedinrecruiter/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.letit0or1.akimaleo.linkedinrecruiter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.letit0or1.akimaleo.linkedinrecruiter.storage.UserUtil
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loginInput.setText(UserUtil.instance.login)
+        passwordInput.setText(UserUtil.instance.password)
+
+        signIn.setOnClickListener {
+            UserUtil.instance.saveData(loginInput.text.toString(), passwordInput.text.toString())
+        }
+    }
+}
